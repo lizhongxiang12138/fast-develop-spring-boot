ff57e78439bada4748495c77946640e5fb1e8e18
953934680@qq.com <953934680@qq.com>
2018-05-07 14:21:29 +0800

    ~~~

--- src/main/java/com/lzx/sys/controller/TestController.java
+++ src/main/java/com/lzx/sys/controller/TestController.java
@@ -1,5 +1,15 @@
 package com.lzx.sys.controller;
 
+import com.baiwang.bop.client.BopException;
+import com.baiwang.bop.client.IBopClient;
+import com.baiwang.bop.client.ILoginClient;
+import com.baiwang.bop.client.impl.BopRestClient;
+import com.baiwang.bop.client.impl.PostLogin;
+import com.baiwang.bop.request.impl.LoginRequest;
+import com.baiwang.bop.request.impl.invoice.common.InvoiceDetails;
+import com.baiwang.bop.request.impl.invoice.impl.InvoiceOpenRequest;
+import com.baiwang.bop.respose.entity.InvoiceOpenResponse;
+import com.baiwang.bop.respose.entity.LoginResponse;
 import com.lzx.sys.config.ZookeeperConfig;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
@@ -13,8 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
@@ -149,6 +159,109 @@ public class TestController {
             }
         });
         return children;
+    }
+
+    public static String url = "http://60.205.83.27/router/rest";             //接口地址
+    public static String appkey="10000005";                                   //AppKey
+    public static String appSecret="b65025d0-19d2-4841-88f4-ff4439b8da58";    //AppSecrect
+    public static String userName="admin_1800000021168";                      //用户名
+    public static String password="a123456";                                   //密码
+    public static String userSalt="94db610c5c3049df8da3e9ac91390015";         //盐值
+
+    /**
+     * 电子发票接口测试
+     * @return
+     */
+    @GetMapping("/electronicInvoice")
+    public String electronicInvoice(){
+        String serialNo = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+        String sellerTaxNo = "91530100795186426Y";
+        try
+        {
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.setAppkey(appkey);
+            loginRequest.setAppSecret(appSecret);
+            loginRequest.setUserName(userName);
+            loginRequest.setPasswordMd5(password);
+            loginRequest.setUserSalt(userSalt);
+
+            ILoginClient loginClient = new PostLogin(url);
+            LoginResponse loginResponse = loginClient.login(loginRequest);
+
+            String token=loginResponse.getAccess_token();//获取token
+
+            InvoiceOpenRequest request = new InvoiceOpenRequest();
+            request.setSellerTaxNo(sellerTaxNo);
+            request.setDeviceType("0");
+            request.setOrganizationCode("");
+            request.setSerialNo(serialNo);
+            request.setInvoiceSpecialMark("00");
+            request.setInvoiceTypeCode("026");
+            request.setInvoiceTerminalCode("004");
+            request.setBuyerTaxNo("");
+            request.setBuyerName("百望股份有限公司");
+            request.setBuyerAddressPhone("");
+            request.setBuyerBankAccount("");
+            request.setDrawer("wwm");
+            request.setChecker("");
+            request.setPayee("");
+            request.setInvoiceType("0");
+            request.setInvoiceListMark("0");
+            request.setRedInfoNo("");
+            request.setOriginalInvoiceCode("");
+            request.setOriginalInvoiceNo("");
+            request.setTaxationMode("0");
+            request.setDeductibleAmount("");
+            request.setInvoiceTotalPrice("200.00");
+            request.setInvoiceTotalTax("34.00");
+            request.setInvoiceTotalPriceTax("234.00");
+            request.setSignatureParameter("0000004282000000");
+            request.setTaxDiskNo("");
+            request.setTaxDiskKey("");
+            request.setTaxDiskPassword("");
+            request.setGoodsCodeVersion("");
+            request.setConsolidatedTaxRate("");
+            request.setNotificationNo("");
+            request.setRemarks("备注");
+
+            List<InvoiceDetails> invoiceDetailsList = new ArrayList<InvoiceDetails>();
+            InvoiceDetails invoiceDetails = new InvoiceDetails();
+            invoiceDetails.setGoodsLineNo("1");
+            invoiceDetails.setGoodsLineNature("0");
+            invoiceDetails.setGoodsCode("1020101000000000000");
+            invoiceDetails.setGoodsExtendCode("");
+            invoiceDetails.setGoodsName("ԭú");
+            invoiceDetails.setGoodsTaxItem("");
+            invoiceDetails.setGoodsSpecification("");
+            invoiceDetails.setGoodsUnit("");
+            invoiceDetails.setGoodsQuantity("");
+            invoiceDetails.setGoodsPrice("");
+            invoiceDetails.setGoodsTotalPrice("200.00");
+            invoiceDetails.setGoodsTotalTax("34.00");
+            invoiceDetails.setGoodsTaxRate("0.17");
+            invoiceDetails.setGoodsDiscountLineNo("");
+            invoiceDetails.setPriceTaxMark("0");
+            invoiceDetails.setVatSpecialManagement("");
+            invoiceDetails.setFreeTaxMark("");
+            invoiceDetails.setPreferentialMark("0");
+            invoiceDetailsList.add(invoiceDetails);
+
+            request.setInvoiceDetailsList(invoiceDetailsList);
+
+            IBopClient client = new BopRestClient(url, appkey, appSecret);
+
+            InvoiceOpenResponse response = 	client.execute(request, token, InvoiceOpenResponse.class);
+            return response.getInvoiceDate();
+        }
+        catch (BopException e)
+        {
+            System.out.println(e.getErrCode());
+            System.out.println(e.getErrMsg());
+            System.out.println(e.getSubMessage());
+            System.out.println(e.getSubCode());
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 }
